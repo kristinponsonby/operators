3c79da318d6c61b8c0c99397166d730917bf5ac1
Kristin <kristinponsonby@gmail.com>
2021-02-08 15:15:02 -0500

    commit tree f021d93b0a83ce247973f5d839cd87828f665940

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,25 @@
 def unsafe?(speed)
-
+    if speed < 60 && speed > 40
+        false
+    elsif speed >= 60 
+        true
+    elsif speed < 40
+        true
+    end
 end
 
-
-
 def not_safe?(speed)
-	
+    if speed < 60 && speed > 40 
+        false
+        speed > 60 ? true : false 
+    elsif speed < 40
+        true
+    elsif speed > 60
+        true
+    
+    end 
+
+
 end
 	
 
